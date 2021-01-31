a9452451e68db807aed827791dc7c65d078717f8
Alexandr <ricko28@ya.ru>
2021-02-01 02:23:54 +0300

    rename files task-1, task-2, task-3

--- lesson-4/task-3.py
+++ lesson-4/task-3.py
@@ -1,3 +1,5 @@
+# 3. Для чисел в пределах от 20 до 240 найти числа, кратные 20 или 21. Необходимо решить задание в одну строку.
+# Подсказка: использовать функцию range() и генератор.
 
-new_list = [index for index in range(20, 241)]
+new_list = [number for number in range(20, 241) if number % 20 == 0 or number % 21 == 0]
 print(new_list)
