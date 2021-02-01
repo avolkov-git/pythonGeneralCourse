05a09e0d235773c3868ded2694bbb91bd039c066
Alexandr <ricko28@ya.ru>
2021-02-01 03:24:05 +0300

    add task-7.py, edit mylib.py

--- lesson-4/mylib.py
+++ lesson-4/mylib.py
@@ -1,3 +1,4 @@
+# Для задания №1 (Функция расчета заработной платы)
 def payroll_calculation(hours, rate):
     try:
         return int(hours) * int(rate)
@@ -6,6 +7,7 @@ def payroll_calculation(hours, rate):
         return 0
 
 
+# Для задания №2 (Функция генератора проверки числа. Проверка что текущее больше предыдущего)
 def max_numbers_list(number_list):
     prev = None
     for number in number_list:
@@ -16,5 +18,16 @@ def max_numbers_list(number_list):
         prev = number
 
 
+# Для задания №5 (Функция для reduce)
 def sum_of_two(rev_element, element):
     return rev_element + element
+
+
+# Для задания №7 (Функция генератора факториала)
+def fact(n):
+    iterator = 1
+
+    for index in range(1, n + 1):
+        # Конструкцию *= подсмотрел на стак оверфлоу
+        iterator *= index
+        yield iterator
