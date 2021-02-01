354170e3e62db545cabd5ef21cb5ac2935843da7
Alexandr <40424525+avolkov-git@users.noreply.github.com>
2021-02-01 03:25:51 +0300

    Delete lesson-1.py

--- lesson-4/lesson-1.py
+++
@@ -1,8 +0,0 @@
-
-from sys import argv
-from mylib import payroll_calculation
-
-script, name, hours, rate = argv
-
-result = payroll_calculation(hours, rate)
-print(f'Employee {name} earned {result}')
