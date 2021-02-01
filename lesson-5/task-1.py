555433f29c8a6e24e74d8eb609dbabae0ba5f6b5
Alexandr <ricko28@ya.ru>
2021-02-01 17:42:58 +0300

    edit task-1.py, edit task-1.txt (Lorem ipsum)

--- lesson-5/task-1.py
+++ lesson-5/task-1.py
@@ -3,7 +3,7 @@
 usr_string = None
 
 with open('task-1.txt', 'w') as txt_file:
-    while usr_string != '':
+    while usr_string or usr_string is None:
         usr_string = input('Введите текстовое сообщение (для выхода введите пустое сообщение): ')
-        if usr_string != '':
+        if usr_string:
             print(usr_string, file=txt_file)
