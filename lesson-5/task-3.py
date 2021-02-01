d9ba0fd6c3e84648c3e403fc33131d53adf43903
Alexandr <ricko28@ya.ru>
2021-02-01 21:40:32 +0300

    add task-3.py, add task-3.txt (salary)

---
+++ lesson-5/task-3.py
@@ -0,0 +1,26 @@
+# 3. Создать текстовый файл (не программно), построчно записать фамилии сотрудников и величину их окладов. Определить,
+# кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников. Выполнить подсчет средней величины
+# дохода сотрудников.
+
+with open('task-3.txt', 'r', encoding='utf-8') as txt_file:
+    # Определение сотрудников с зарплатой менее 20 000
+    salary_less_20k = []
+    # Переменная для подсчета средней заработной платы
+    salary_sum = 0
+    # Количество человек в списке
+    lines_count = 0;
+    # lines_count = len(txt_file.readlines())
+    # Вопрос: почему, если я раскомментирую строку выше, перестанет работать лямбда?
+
+    for txt_line in map(lambda line: line.rstrip('\n'), txt_file):
+        spl_line = txt_line.split(',')
+        salary_sum += int(spl_line[1])
+        lines_count += 1
+
+        if int(spl_line[1]) < 20000:
+            salary_less_20k.append(spl_line[0])
+
+    print(f'Сотрудники с окладом менее 20000: {salary_less_20k}')
+
+    salary_avg = salary_sum / lines_count
+    print(f'Средняя зарплата сотрудников: {salary_avg}')
