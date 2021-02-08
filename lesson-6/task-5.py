db565b86d90c15b3a0597ab38bf6d097eba467f2
Alexandr <ricko28@ya.ru>
2021-02-08 23:19:19 +0300

    add task-5.py

---
+++ lesson-6/task-5.py
@@ -0,0 +1,36 @@
+# 5. Реализовать класс Stationery (канцелярская принадлежность). Определить в нем атрибут title (название) и метод
+# draw (отрисовка). Метод выводит сообщение “Запуск отрисовки.” Создать три дочерних класса Pen (ручка), Pencil
+# (карандаш), Handle (маркер). В каждом из классов реализовать переопределение метода draw. Для каждого из классов
+# методы должен выводить уникальное сообщение. Создать экземпляры классов и проверить, что выведет описанный метод
+# для каждого экземпляра.
+
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print('Запуск отрисовки')
+
+
+class Pen(Stationery):
+    def draw(self):
+        print('Запуск отрисовки ручкой')
+
+
+class Pencil(Stationery):
+    def draw(self):
+        print('Запуск отрисовки карандашом')
+
+
+class Handle(Stationery):
+    def draw(self):
+        print('Запуск отрисовки маркером')
+
+
+new_pen = Pen('Ручка')
+new_pencil = Pencil('Карандаш')
+new_handle = Handle('Маркер')
+
+new_pen.draw()
+new_pencil.draw()
+new_handle.draw()
